b5a2ca88cff5fc8a3cdcced93f6805f2c06fa861
S. C. Arzehd <scarzehd@gmail.com>
2025-06-11 21:17:06 -0400

    Inconsequential change to test the build system

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _ready() -> void:
 	DialogueManager.show_dialogue_balloon(load("res://resources/dialog/test.dialogue"))
+	
+	await DialogueManager.dialogue_ended
+	
+	get_tree().quit()
